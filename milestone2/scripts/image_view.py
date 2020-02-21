790058bbd1c044a1c162181e57ed203aab2588c2
lindabui <lbui@kth.se>
2020-02-21 17:11:32 +0100

    generate undistorded images from video for training

--- milestone2/scripts/image_view.py
+++ milestone2/scripts/image_view.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
+import numpy as np
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import roslib
 import rospy
 from sensor_msgs.msg import Image
+import os.path
+import glob
+
 
 #publishe the camera_raw images at a slower rate
 
 image_msg = None
 bridge = CvBridge()
+mtx  = np.load('mtx.npy')
+dist = np.load('dist.npy')
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(640,480),1,(640,480))
 
 def callback(msg):
     global image_msg
@@ -26,21 +33,40 @@ rospy.init_node('traning_data')
 
 def main():
     global image_msg
-    rate = rospy.Rate(1) #Hz
-    i=1
+    rate = rospy.Rate(10) #Hz
+    files = glob.glob("/home/robot/Traning_set/*.png")
+    files.sort()
+    i = 0
+    print(files)
+    if len(files) != 0 :
+        string = files[-1].split('/')[-1]
+        i = (int(string[5:-4]))+1
+    print(i)
+    
+
+
 
     while not rospy.is_shutdown():
-        k = input("Press qny key to capture image")
+        raw_input("Press any key to capture image ")
         if image_msg:
             try:
                 img = bridge.imgmsg_to_cv2(image_msg, 'bgr8')
             except CvBridgeError as e:
                 print(e)
             image_msg = None
-            path = "~/Traning_set/image"+str(i)+".png"
+            #get undistorted image
+            dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+            x,y,w,h = roi
+            dst = dst[y:y+h, x:x+w]
+
+            n_0 = 4 - len(str(i))
+            n = '0' * n_0
+
+            filename = 'image' + n + str(i) + '.png'
+            path = os.path.join(os.path.expanduser('~'),'Traning_set',filename)
             i+=1
-            cv2.imwrite(path, img)
-            print("image_pub saved!")
+            if not cv2.imwrite(path, dst):
+                raise Exception("Could not write image")
         rate.sleep()
 
 if __name__ == '__main__':
