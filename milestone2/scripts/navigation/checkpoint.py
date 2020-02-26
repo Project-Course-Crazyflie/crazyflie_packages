ae93b3e3503d57cd8a145d4983da84f78797e175
Ivan Appelsved <ivanapp@kth.se>
2020-02-26 22:21:35 +0100

    improving checkpoint, yaw_to_frame still doesn't work

--- milestone2/scripts/navigation/checkpoint.py
+++ milestone2/scripts/navigation/checkpoint.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
-#import numpy as np
 #import cv2
 #from cv_bridge import CvBridge, CvBridgeError
 import roslib
 import rospy
 import tf2_ros
 from std_msgs.msg import Bool, String
-from geometry_msgs.msg import PoseStamped
+from tf2_geometry_msgs import PoseStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
-from aruco_follower import ArucoFollower
 import math
+import numpy as np
 from time import sleep
 #def yaw_towards_frame(cf1_pose, target_frame, transform) returns list of quaternion point in frame cf1/odom
 
@@ -60,8 +59,6 @@ pose_pub = rospy.Publisher('cf1/move_to', PoseStamped, queue_size=10)
 tf_buf   = tf2_ros.Buffer()
 tf_lis = tf2_ros.TransformListener(tf_buf, queue_size=100)
 
-aruco_thing = ArucoFollower()
-
 
 def main():
 	rospy.logwarn('in main')
@@ -76,6 +73,7 @@ def main():
 			rot_to = rotate_to
 			rotate_to = None
 			#then do the following
+			rospy.loginfo(type(curr_pose))
 
 			#rotate 2 thirds of a full rotation
 			rospy.logwarn('starting rotation')
@@ -100,8 +98,10 @@ def main():
 				rate.sleep()
 				continue
 			"""
-			transform = tf_buf.lookup_transform('cf1/base_link',rot_to.data, rospy.Time(0))
-			quats = aruco_thing.yaw_towards_frame(curr_pose, rot_to.data, transform)
+			#transform = tf_buf.lookup_transform('cf1/base_link',rot_to.data, rospy.Time(0))
+			#transform = tf_buf.lookup_transform(rot_to.data,'cf1/base_link', rospy.Time(0))
+			quats = yaw_towards_frame(curr_pose, rot_to.data)
+			rospy.loginfo(euler_from_quaternion(quats))
 
 			rotate = PoseStamped()
 			rotate.header.frame_id = 'cf1/base_link'
@@ -117,12 +117,41 @@ def main():
 			pose_pub.publish(rotate)
 
 
-			rospy.sleep(sleep_time_in_rotation*4)
+			rospy.sleep(sleep_time_in_rotation*40)
 
 
 			done_publish()
 		rate.sleep()
 	return
+
+def yaw_towards_frame(pose, target_frame):
+	"""Return the yaw towards frame represented in a quaternion (list)
+	"""
+	# TODO: send in transform tf_buf as argument instead of calculating here
+	if not tf_buf.can_transform(target_frame, 'map', rospy.Time(0)):
+		rospy.logwarn_throttle(5.0, 'No transform from %s to map' % target_frame)
+		return
+	rospy.loginfo(['-'*20,pose])
+	if pose.header.frame_id != "map":
+		pose = tf_buf.transform(pose, 'map')
+
+	frame_pose = PoseStamped() # [0, 0, 0]
+	frame_pose.header.stamp = rospy.Time(0)
+	frame_pose.header.frame_id = target_frame
+
+	#rospy.loginfo(tf_buf.registration.print_me())
+
+	p = tf_buf.transform(frame_pose, 'map')
+
+	frame_pos = np.array([p.pose.position.x, p.pose.position.y, p.pose.position.z])
+	curr = np.array([pose.pose.position.x, pose.pose.position.y, pose.pose.position.z])
+
+	diff = frame_pos-curr
+	theta = np.arctan2(diff[1], diff[0])
+
+	q_rot = quaternion_from_euler(0, 0, theta,"sxyz")
+
+	return q_rot
 
 if __name__ == '__main__':
 	main()
