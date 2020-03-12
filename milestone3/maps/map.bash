60c9d31b19f4706e3284b72aef080283832e0e54
Tom <ivanapp@kth.se>
2020-03-12 19:53:26 +0100

    made it executable, nothing else

--- milestone3/maps/map.bash
+++ milestone3/maps/map.bash
@@ -1,8 +1,10 @@
 echo 'creating link, this requires superuser priviliges'
-sudo ln -s ~/dd2419_ws/src/course_packages/dd2419_resources/worlds_json ~/dd2419_ws/src/crazyflie_packages/milestone3/
+LINKPATH=~/dd2419_ws/src/crazyflie_packages/milestone3/
+WORLDPATH=~/dd2419_ws/src/course_packages/dd2419_resources/worlds_json
+sudo ln -s $WORLDPATH $LINKPATH
 
 echo 'copying files'
-cp ./* ../worlds_json/ && rm ../worlds_json/map.bash || echo 'the link was not created properly, please ask Ivan what to do.'
+cp $LINKPATH/maps/* $LINKPATH/worlds_json/ && rm $LINKPATH/worlds_json/map.bash || echo 'the link was not created properly, please ask Ivan what to do.'
 
 echo 'appending the link to git-ignore'
 echo 'worlds_json' >> ~/dd2419_ws/src/crazyflie_packages/.git/info/exclude
