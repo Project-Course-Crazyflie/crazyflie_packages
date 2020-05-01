045bef19578ed1505f10ade7715f97aeaa325569
lindabui <lbui@kth.se>
2020-05-01 17:41:51 +0200

    small changes in draw_graph

--- milestone3/scripts/path_planning/RRT_ROS_3D.py
+++ milestone3/scripts/path_planning/RRT_ROS_3D.py
@@ -293,8 +293,6 @@ class RRT:
 			for xx, yy, zz in zip(node.x_path, node.y_path, node.z_path):
 				point = np.array([xx, yy, zz])
 
-				check_plane = np.dot(point, normal) + d
-
 				point_plane_distance = abs((np.dot(normal, point) + d)/math.sqrt(normal[0]**2 + normal[1]**2 + normal[2]**2))
 				#print("distance: " + str(point_plane_distance))
 				if point_plane_distance < self.inflation:
@@ -325,10 +323,16 @@ class RRT:
 
 			d = -corner.dot(normal)
 
-			yy, zz = np.meshgrid(np.linspace(start[1], stop[1], 3) , np.linspace(start[2], stop[2], 3))
-			xx = (-normal[1]*yy - normal[2]*zz - d)*1./normal[0]
+			if normal[0] == 0.0:
+				xx, zz = np.meshgrid(np.linspace(start[0], stop[0], 2) , np.linspace(start[2], stop[2], 2))
+				yy = (-normal[0]*xx - normal[2]*zz - d)*1./normal[1]
 
-			fig.plot_surface(xx, yy, zz)
+				fig.plot_surface(xx, yy, zz)
+			elif normal[1] == 0.0:
+				yy, zz = np.meshgrid(np.linspace(start[1], stop[1], 2) , np.linspace(start[2], stop[2], 2))
+				xx = (-normal[1]*yy - normal[2]*zz - d)*1./normal[0]
+
+				fig.plot_surface(xx, yy, zz)
 
 		fig.scatter(self.start_node.x, self.start_node.y, self.start_node.z, "xr")
 		fig.scatter(self.goal_node.x, self.goal_node.y, self.goal_node.z, "xr")
